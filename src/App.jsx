28d3b91108d72802dc3af0199d3bb5ac822f2dcd
Channa <tobchanna188@gmail.com>
2026-06-09 23:17:43 +0700

    Initial React + Vite + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-green-600 text-white text-3xl">
+      Tailwind Works 🚀
+    </div>
+  )
+}
+
+export default App
